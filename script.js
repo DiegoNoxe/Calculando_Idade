827de1a593fcb206050ca5d2a4013abbeec1669b
Diego <131261339+DiegoNoxe@users.noreply.github.com>
2024-08-18 13:14:03 -0300

    Testando option
    - Testando troca de idioma com o select e javascript.

--- script.js
+++ script.js
@@ -1,3 +1,58 @@
+const translations = {
+    'pt-br': {
+        title: 'Calculadora de Idade',
+        anoAtualPlaceholder: 'Digite o ano atual',
+        anoNascimentoPlaceholder: 'Data de nascimento',
+        enviarButton: 'Enviar',
+        limparButton: 'Limpar',
+        resultadoText: '',
+        pe: 'Feito por Diego'
+    },
+    'en': {
+        title: 'Age Calculator',
+        anoAtualPlaceholder: 'Enter the current year',
+        anoNascimentoPlaceholder: 'Year of birth',
+        enviarButton: 'Submit',
+        limparButton: 'Clear',
+        resultadoText: '',
+        pe: 'Made by: Diego'
+    }
+};
+    
+    
+function updateText(language) {
+    const text = translations[language];
+    
+    document.getElementById('title').innerText = text.title;
+    document.getElementById('anoAtual').placeholder = text.anoAtualPlaceholder;
+    document.getElementById('anoNascimento').placeholder = text.anoNascimentoPlaceholder;
+    document.getElementById('Enviar').innerText = text.enviarButton;
+    document.getElementById('Limpar').innerText = text.limparButton;
+    document.getElementById('resultado').innerText = text.resultadoText;
+    document.getElementById('pe').innerText = text.pe;
+}
+
+
+document.getElementById('languageS').addEventListener('change', (event) => {
+    updateText(event.target.value);
+});
+
+updateText('pt-br');
+    
+    
+    
+    
+    
+    
+
+    
+    
+
+
+
+
+
+    
     const anoAtualInput = document.getElementById('anoAtual'); 
     const anoNascimentoInput = document.getElementById('anoNascimento'); 
     const enviarButton = document.getElementById('Enviar');
