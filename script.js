7cda87a3d6c3f0b20385ac1207beb73d1e843e1c
Diego <Diegofla10@outlook.com>
2024-10-15 20:16:16 -0300

    cc

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const translations = {
+ const translations = {
     'pt-br': {
+        tittle: 'Calculadora de Idade',
         anoAtualPlaceholder: 'Digite o ano atual',
         anoNascimentoPlaceholder: 'Data de nascimento',
         enviarButton: 'Enviar',
@@ -8,6 +9,7 @@ const translations = {
         pe: 'Feito por <span class="corz"><a target="_blank" href="https://github.com/DiegoNoxe">Diego Nascimento</a</span>'
     },
     'en': {
+        tittle: 'Age Calculator',
         anoAtualPlaceholder: 'Enter the current year',
         anoNascimentoPlaceholder: 'Year of birth',
         enviarButton: 'Submit',
@@ -18,9 +20,11 @@ const translations = {
 };
     
     
-function updateText(language) {
-    const text = translations[language];
+function updateText(language) 
 
+{
+    const text = translations[language];
+    document.getElementById('tituloo').innerHTML = text.tittle;
     document.getElementById('anoAtual').placeholder = text.anoAtualPlaceholder;
     document.getElementById('anoNascimento').placeholder = text.anoNascimentoPlaceholder;
     document.getElementById('Enviar').innerText = text.enviarButton;
@@ -36,45 +40,39 @@ document.getElementById('languageS').addEventListener('change', (event) => {
 
 updateText('pt-br');
     
-    
-    
-    
-    
-    
-
-    
-    
 
 
 
-
-
-    
-    const anoAtualInput = document.getElementById('anoAtual'); 
+    const anoAtualInput      = document.getElementById('anoAtual'); 
     const anoNascimentoInput = document.getElementById('anoNascimento'); 
-    const enviarButton = document.getElementById('Enviar');
-    const resultado = document.getElementById('resultado');
-    const limparButton = document.getElementById('Limpar');
+    const enviarButton       = document.getElementById('Enviar');
+    const resultado          = document.getElementById('resultado');
+    const limparButton       = document.getElementById('Limpar');
+
+
 
         function calcularIdade() 
         {
 
-            const anoAtual = parseInt(anoAtualInput.value);
-            const anoNascimento = parseInt(anoNascimentoInput.value);
-            const idade = anoAtual - anoNascimento;
+            const anoAtual      = Number(anoAtualInput.value);
+            const anoNascimento = Number(anoNascimentoInput.value);
+            const idade         = anoAtual - anoNascimento;
 
             if(anoAtual < anoNascimento)
             
             {
                 resultado.textContent = "Idade maior que o ano atual!"
+
                 resultado.classList.add('error');
                 resultado.classList.remove('result');
                 return;
             }
 
             if (isNaN(anoAtual) || isNaN(anoNascimento)) {
-                 resultado.classList.add('validos');
+
+                resultado.classList.add('validos');
                 resultado.classList.remove('result');
+
                 resultado.innerHTML = 'Por favor, insira apenas números <span class="valido">válidos</span>.';
                 return;
             }
@@ -97,14 +95,23 @@ updateText('pt-br');
                 return;
             }
 
+            if(idade == 1)
+            {
+                resultado.textContent = `Você têm ${idade} ano de idade!`;
+                return;
+            }
 
-            resultado.textContent = `Você têm ${idade} de idade!`;
+            resultado.textContent = `Você têm ${idade} anos de idade!`;
         }
 
-        function limpeza() {
-            anoAtualInput.value = ''; anoNascimentoInput.value = ''; resultado.textContent = ''; 
-            resultado.classList.remove('error', 'errorTwo', 'result');
+            function limpeza() {
+
+            anoAtualInput.value = ''; 
+            anoNascimentoInput.value = ''; 
+            resultado.textContent = '';
         }
+
+
 
         enviarButton.addEventListener('click', calcularIdade);
         limparButton.addEventListener('click', limpeza);
